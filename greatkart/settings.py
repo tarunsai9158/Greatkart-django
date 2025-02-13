8f279237a1297fde5e28e1f78ed5451d648b3c09
peela trarun sai <tarunpeela1@gmail.com>
2025-02-13 11:41:51 +0530

    store and cart functionality

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'category',
     'accounts',
     'store',
+    'cart',
 ]
 
 MIDDLEWARE = [
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_list',
+                'cart.context_processors.counter',
             ],
         },
     },
--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,8 @@ from .views import *
 
 urlpatterns = [
     path('',store, name='store'),
-    path('<slug:category_slug>/', store, name= 'products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>/', product_detail, name= 'product_detail'),
+    path('category/<slug:category_slug>/', store, name= 'products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>/', product_detail, name= 'product_detail'),
+    path('search/', search, name='search')
     
 ]
