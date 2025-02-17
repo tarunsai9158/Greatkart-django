28f994620d00ae1b2a5a74d6988faf2b4b3dc724
peela trarun sai <tarunpeela1@gmail.com>
2025-02-17 09:59:48 +0530

    Django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -143,3 +143,19 @@ MEDIA_ROOT = BASE_DIR /'media'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+
+# SMTP CONFIGURATION
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'tarunpeela1@gmail.com'  # Replace with your Gmail
+EMAIL_HOST_PASSWORD = 'mdmz fcxk zohm tlvq'  # Replace with your App Password
--- greatkart/urls.py
+++ greatkart/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('',home, name='home'),
     path('store/', include('store.urls')),
     path('cart/', include('cart.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
